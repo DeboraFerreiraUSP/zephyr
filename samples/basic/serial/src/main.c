3afedcace9fd444fa2e7788ec0496be838a219fc
DeboraFerreiraUSP <deborabferreira@usp.br>
2024-12-06 19:07:49 -0300

    mudanças do dia 06/12

--- samples/basic/serial/src/main.c
+++ samples/basic/serial/src/main.c
@@ -14,7 +14,8 @@
 
 K_FIFO_DEFINE (envia);
 K_FIFO_DEFINE (recebe);
-
+K_MUTEX_DEFINE (mutex_tx);
+K_CONDVAR_DEFINE (condvar_tx);
 struct pacote {
 	void *fifo_reserved; /* 1st word reserved for use by fifo */
 	uint8_t u;
@@ -24,6 +25,8 @@ struct pacote {
 	uint8_t pos;
 	size_t value;
 };
+
+const struct device *sb = DEVICE_DT_GET(DT_NODELABEL(gpiob));
 
 
 void teclado (void){
@@ -52,8 +55,60 @@ void teclado (void){
 }
 
 
-void leitura_rx(void){
-	struct pacote *pacote_leitura;
-	pacote_leitura = k_fifo_get(&recebe, K_FOREVER);
+void tx(void){
+	struct pacote *tx;
+	int aux=0;
+	gpio_pin_set(sb, 0x3, aux);
+	if (tx = k_fifo_get(&envia, K_FOREVER)!=NULL){
+		for (int i=1; i<= 8; i++){
+			aux = (*tx).u && 0b1;
+			(*tx).u = (*tx).u >> 1;
+			k_msleep(10);
+			gpio_pin_set(sb, 0x3, aux);
+		}for (int i=1; i<= 8; i++){
+			aux = (*tx).sync && 0b1;
+			(*tx).sync = (*tx).sync >> 1;
+			k_msleep(10);
+			gpio_pin_set(sb, 0x3, aux);
+		}for (int i=1; i<= 8; i++){
+			aux = (*tx).cabecalho && 0b1;
+			(*tx).cabecalho = (*tx).cabecalho >> 1;
+			k_msleep(10);
+			gpio_pin_set(sb, 0x3, aux);
+		}for (int i=0; i<((*tx).cabecalho && 0b111); i++) {
+			for (int j=0; j<8;j++) {
+				aux = (*tx).frase[i] && 0b1;
+				(*tx).frase[i] = (*tx).frase[i] >> 1;
+				k_msleep(10);
+				gpio_pin_set(sb, 0x3, aux);
+		}
+		}for (int i=1; i<= 8; i++){
+			aux = (*tx).cabecalho && 0b1;
+			(*tx).cabecalho = (*tx).cabecalho >> 1;
+			k_msleep(10);
+			gpio_pin_set(sb, 0x3, aux);
+		}for (int i=1; i<= 8; i++){
+			aux = (*tx).pos && 0b1;
+			(*tx).pos = (*tx).pos >> 1;
+			k_msleep(10);
+			gpio_pin_set(sb, 0x3, aux);
+		}
+			aux = 0;
+			gpio_pin_set(sb, 0x3, aux);
+		}
+}
+
+
+
+void rx(void) {
+	int32_t buffer;
+	struct pacote rx;
 	
+	while (buffer != 0b0101010101) {
+		k_msleep();
+	}
+}
+void main(void) {
+	gpio_pin_configure(sb, 0x3, GPIO_OUTPUT_ACTIVE);
+	gpio_pin_configure(sb, 0x2, GPIO_INPUT);
 }
